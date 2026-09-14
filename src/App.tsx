8640008048fb232e5ab043577ce30aeda8d6cd82
amarktainetwork-blip <amarktainetwork@gmail.com>
2026-09-14 18:33:02 +0200

    Add multi-page Kiddo marketing routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,17 @@ import Chat from './pages/Chat';
 import Library from './pages/Library';
 import Parent from './pages/Parent';
 import { Contact, Privacy, Terms } from './pages/Legal';
+import { Creativity, HowItWorks, ParentsInfo, SafetyInfo } from './pages/MarketingPages';
 
 export default function App(){
   return <BrowserRouter><Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/login" element={<Login/>}/>
     <Route path="/register" element={<Register/>}/>
+    <Route path="/how-it-works" element={<HowItWorks/>}/>
+    <Route path="/creativity" element={<Creativity/>}/>
+    <Route path="/for-parents" element={<ParentsInfo/>}/>
+    <Route path="/safety" element={<SafetyInfo/>}/>
     <Route path="/privacy" element={<Privacy/>}/>
     <Route path="/terms" element={<Terms/>}/>
     <Route path="/contact" element={<Contact/>}/>
